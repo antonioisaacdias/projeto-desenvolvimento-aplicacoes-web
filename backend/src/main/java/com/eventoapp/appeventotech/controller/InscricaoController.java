0ca571cede6bce8c0c44f0a0d6900b54db3fefe7
marcosads2025 <marcosads2025@gmail.com>
2026-04-27 21:31:45 -0300

    integração front end  com back end

--- backend/src/main/java/com/eventoapp/appeventotech/controller/InscricaoController.java
+++ backend/src/main/java/com/eventoapp/appeventotech/controller/InscricaoController.java
@@ -5,13 +5,11 @@ import com.eventoapp.appeventotech.model.Inscricao;
 import com.eventoapp.appeventotech.service.InscricaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/inscricoes")
+@CrossOrigin("*")
 public class InscricaoController {
 
     @Autowired
--- backend/src/main/java/com/eventoapp/appeventotech/model/Inscricao.java
+++ backend/src/main/java/com/eventoapp/appeventotech/model/Inscricao.java
@@ -1,12 +1,7 @@
 package com.eventoapp.appeventotech.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
 
-@Data
 @Entity
 public class Inscricao {
 
@@ -18,4 +13,38 @@ public class Inscricao {
     private String email;
     private String telefone;
 
+    public Inscricao() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
